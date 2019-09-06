720702b1c6e441d6c4adee7783368d49fb964de5
Tyler Denton <tylerdenton@Admins-MacBook-Pro.local>
2019-09-05 17:09:26 -0700

    added cluster stats

--- src/application/main.py
+++ src/application/main.py
@@ -24,7 +24,7 @@ def basic_authorization(user, password):
     s = user + ":" + password
     return "Basic " + s.encode("base64").rstrip()
 
-def _getNodes(url, user, passwrd):
+def _getCluster(url, user, passwrd):
     _url = "http://{}:8091/pools/default".format(url)
     req = urllib2.Request(_url,
                           headers={
@@ -41,13 +41,141 @@ def _getNodes(url, user, passwrd):
     stats = json.loads(f)
     for node in stats['nodes']:
         node_list.append(node['hostname'])
-    return(node_list)
+    result = {}
+    result['nodeList'] = node_list
+    result['metrics'] = []
+    try:
+        if stats['rebalanceStatus'] == "none":
+            result['metrics'].append("{} {{type=\"cluster\"}} {}".format("rebalanceStatus", 0))
+        else:
+            result['metrics'].append("{} {{type=\"cluster\"}} {}".format("rebalanceStatus", 1))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("maxBucketCount", stats['maxBucketCount']))
+    except Exception as e:
+        pass
+    try:
+        int(stats['autoCompactionSettings']['databaseFragmentationThreshold']['percentage'])
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("databaseFragmentationThresholdPercent", stats['autoCompactionSettings']['databaseFragmentationThreshold']['percentage']))
+    except Exception as e:
+        pass
+    try:
+        int(stats['autoCompactionSettings']['databaseFragmentationThreshold']['size'])
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("databaseFragmentationThresholdSize", stats['autoCompactionSettings']['databaseFragmentationThreshold']['size']))
+    except Exception as e:
+        pass
+
+    try:
+        int(stats['autoCompactionSettings']['viewFragmentationThreshold']['percentage'])
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("viewFragmentationThresholdPercent", stats['autoCompactionSettings']['viewFragmentationThreshold']['percentage']))
+    except Exception as e:
+        pass
+    try:
+        int(stats['autoCompactionSettings']['viewFragmentationThreshold']['size'])
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("viewFragmentationThresholdSize", stats['autoCompactionSettings']['viewFragmentationThreshold']['size']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("indexFragmentationThresholdPercent", stats['autoCompactionSettings']['indexFragmentationThreshold']['percentage']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("indexFragmentationThresholdSize", stats['autoCompactionSettings']['indexFragmentationThreshold']['size']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("rebalanceSuccess", stats['counters']['rebalance_success']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("rebalanceStart", stats['counters']['rebalance_start']))
+    except Exception as e:
+        pass
+    try:
+        if stats['balanced'] == True:
+            result['metrics'].append("{} {{type=\"cluster\"}} {}".format("balanced", 0))
+        else:
+            result['metrics'].append("{} {{type=\"cluster\"}} {}".format("balanced", 0))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("memoryQuota", stats['memoryQuota']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("indexMemoryQuota", stats['indexMemoryQuota']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ftsMemoryQuota", stats['ftsMemoryQuota']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("cbasMemoryQuota", stats['cbasMemoryQuota']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("eventingMemoryQuota", stats['eventingMemoryQuota']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramTotal", stats['storageTotal']['ram']['total']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramQuotaTotal", stats['storageTotals']['ram']['quotaTotal']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramQuotaUsed", stats['storageTotals']['ram']['quotaUsed']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramUsed", stats['storageTotals']['ram']['used']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramUsedByData", stats['storageTotals']['ram']['usedByData']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramQuotaUsedPerNode", stats['storageTotals']['ram']['quotaUsedPerNode']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("ramQuotaTotalPerNode", stats['storageTotals']['ram']['quotaTotalPerNode']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("hddTotal", stats['storageTotals']['hdd']['total']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("hddQuotaTotal", stats['storageTotals']['hdd']['quotaTotal']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("hddUsed", stats['storageTotals']['hdd']['used']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("hddUsedByData", stats['storageTotals']['hdd']['usedByData']))
+    except Exception as e:
+        pass
+    try:
+        result['metrics'].append("{} {{type=\"cluster\"}} {}".format("hddFree", stats['storageTotals']['hdd']['free']))
+    except Exception as e:
+        pass
+
+    return(result)
 
 def _get_base_metrics(url, user, passwrd):
     try:
-        metrics = []
-        nodes = _getNodes(url, user, passwrd);
-        for node in nodes:
+
+        result = _getCluster(url, user, passwrd)
+        metrics = result['metrics']
+        for node in result['nodeList']:
             url = node.split(":")[0]
             _url = "http://{}:8091/pools/default".format(url)
             req = urllib2.Request(_url,
@@ -64,7 +192,7 @@ def _get_base_metrics(url, user, passwrd):
             f_json = json.loads(f)
 
             for _node in f_json['nodes']:
-                dashUrl = url.replace(".", "-")
+                # dashUrl = url.replace(".", "-")
                 try:
                     if _node['thisNode']:
                         try:
