5752918eb72261b33299671c8c3d0edf5e2ec08c
Tyler Denton <tylerdenton@Admins-MacBook-Pro.local>
2019-09-27 07:41:12 -0700

    Added bucket, index, query and eventing timestamp metrics

--- src/application/main_ts.py
+++ src/application/main_ts.py
@@ -183,12 +183,68 @@ def _get_bucket_metrics(url, user, passwrd, nodeList):
         f = (urllib2.urlopen(req)).read()
         f_json = json.loads(f)
         for node in nodeList:
-            for bucket in f_json:
-                bucket_info['buckets'].append(bucket['name'])
-                bucket_url = "http://{}:8091/pools/default/buckets/{}/nodes/{}/stats".format(url, bucket['name'], node)
-                print(bucket_url)
-                # print(bucket_url)
-                req = urllib2.Request(bucket_url,
+            try:
+                for bucket in f_json:
+                    bucket_info['buckets'].append(bucket['name'])
+                    bucket_url = "http://{}:8091/pools/default/buckets/{}/nodes/{}/stats".format(url, bucket['name'], node)
+                    print(bucket_url)
+                    req = urllib2.Request(bucket_url,
+                                          headers={
+                                              "Authorization": basic_authorization(user, passwrd),
+                                              "Content-Type": "application/x-www-form-urlencoded",
+
+                                              # Some extra headers for fun
+                                              "Accept": "*/*", # curl does this
+                                              "User-Agent": "check_version/1", # otherwise it uses "Python-urllib/..."
+                                          })
+
+                    b = (urllib2.urlopen(req)).read()
+                    b_json = json.loads(b)
+                    _node = convert_ip_to_string(node)
+                    for record in  b_json['op']['samples']:
+                        if record != "timestamp":
+                            for idx, datapoint in enumerate(b_json['op']['samples'][record]):
+                                bucket_info['metrics'].append("{} {{bucket=\"{}\", node=\"{}\", type=\"bucket\"}} {} {}".format(record, bucket['name'], _node, datapoint, b_json['op']['samples']['timestamp'][idx]))
+            except Exception as e:
+                pass
+    except Exception as e:
+        pass
+    return bucket_info
+
+def _get_index_metrics(url, user, passwrd, nodes, buckets):
+    index_info = {}
+    index_info['metrics'] = []
+    # get cluster index info
+    for node in nodes:
+        _index_url = "http://{}/pools/default/buckets/@index/nodes/{}/stats".format(node, node)
+        try:
+            req = urllib2.Request(_index_url,
+                                  headers={
+                                      "Authorization": basic_authorization(user, passwrd),
+                                      "Content-Type": "application/x-www-form-urlencoded",
+
+                                      # Some extra headers for fun
+                                      "Accept": "*/*", # curl does this
+                                      "User-Agent": "check_version/1", # otherwise it uses "Python-urllib/..."
+                                  })
+
+            i = (urllib2.urlopen(req)).read()
+            i_json = json.loads(i)
+            _node = convert_ip_to_string(node)
+            for record in i_json['op']['samples']:
+                if record != "timestamp":
+                    for idx, datapoint in enumerate(i_json['op']['samples'][record]):
+                        index_info['metrics'].append("{} {{node=\"{}\", type=\"index\"}} {} {}".format(record, _node, datapoint, i_json['op']['samples']['timestamp'][idx]))
+
+        except Exception as e:
+            print("index base: " + str(e))
+
+    for node in nodes:
+        for bucket in buckets:
+            try:
+                index_info_url = "http://{}/pools/default/buckets/@index-{}/nodes/{}/stats".format(node, bucket, node)
+                print(index_info_url)
+                req = urllib2.Request(index_info_url,
                                       headers={
                                           "Authorization": basic_authorization(user, passwrd),
                                           "Content-Type": "application/x-www-form-urlencoded",
@@ -198,17 +254,120 @@ def _get_bucket_metrics(url, user, passwrd, nodeList):
                                           "User-Agent": "check_version/1", # otherwise it uses "Python-urllib/..."
                                       })
 
-                b = (urllib2.urlopen(req)).read()
-                b_json = json.loads(b)
+                ii = (urllib2.urlopen(req)).read()
+                ii_json = json.loads(ii)
+                for record in ii_json['op']['samples']:
+                    name = ""
+                    index_type=""
+                    stat = ""
+                    _node = convert_ip_to_string(node)
+                    try:
+                        split_record = record.split("/")
+                        if len(split_record) == 3:
+                            name = (split_record[1]).replace("+", "_")
+                            index_type = (split_record[2]).replace("+", "_")
+                            if type(ii_json['op']['samples'][record]) == type([]):
+                                for idx, datapoint in enumerate(ii_json['op']['samples'][record]):
+                                    index_info['metrics'].append("{} {{node = \"{}\", index=\"{}\", bucket=\"{}\", type=\"index_stat\"}} {} {}".format(index_type, _node, name, bucket, datapoint, ii_json['op']['samples']['timestamp'][idx]))
+                            else:
+                                index_info['metrics'].append("{} {{node = \"{}\", index=\"{}\", bucket=\"{}\", type=\"index_stat\"}} {}".format(index_type, _node, name, bucket, ii_json['op']['samples'][record]))
+
+                        elif len(split_record) == 2:
+                            index_type = split_record[1]
+                            if type(ii_json['op']['samples'][record]) == type([]):
+                                for idx, datapoint in enumerate(ii_json['op']['samples'][record]):
+                                    index_info['metrics'].append("{} {{node = \"{}\", bucket=\"{}\", type=\"index_stat\"}} {} {}".format(index_type, _node, bucket, datapoint, ii_json['op']['samples']['timestamp'][idx]))
+                            else:
+                                index_info['metrics'].append("{} {{node = \"{}\", bucket=\"{}\", type=\"index_stat\"}} {}".format(index_type, _node, bucket, ii_json['op']['samples'][record]))
+                        else:
+                            next
+                    except Exception as e:
+                        print("index specific: " + str(e))
+
+            except Exception as e:
+                print("index: " + str(e))
+
+    return index_info
+
+def _get_query_metrics(url, user, passwrd, nodeList):
+    query_info = {}
+    query_info['metrics'] = []
+
+    for node in nodeList:
+        try:
+            _query_url = "http://{}/pools/default/buckets/@query/nodes/{}/stats".format(node, node)
+            req = urllib2.Request(_query_url,
+                                  headers={
+                                      "Authorization": basic_authorization(user, passwrd),
+                                      "Content-Type": "application/x-www-form-urlencoded",
+
+                                      # Some extra headers for fun
+                                      "Accept": "*/*", # curl does this
+                                      "User-Agent": "check_version/1", # otherwise it uses "Python-urllib/..."
+                                  })
+
+            q = (urllib2.urlopen(req)).read()
+            q_json = json.loads(q)
+            _node = convert_ip_to_string(node)
+            for record in q_json['op']['samples']:
+                if record != "timestamp":
+                    for idx, datapoint in enumerate(q_json['op']['samples'][record]):
+                        query_info['metrics'].append("{} {{node = \"{}\", type=\"query\"}} {} {}".format(record, _node, datapoint, q_json['op']['samples']['timestamp'][idx]))
+
+        except Exception as e:
+            print("query base: " + str(e))
+    return query_info
+
+
+def _get_eventing_metrics(url, user, passwrd, nodeList):
+    eventing_metrics = {}
+    eventing_metrics['metrics'] = []
+
+    for node in nodeList:
+        try:
+            _event_url = "http://{}/pools/default/buckets/@eventing/nodes/{}/stats".format(node, node)
+            print(_event_url)
+            req = urllib2.Request(_event_url,
+                                  headers={
+                                      "Authorization": basic_authorization(user, passwrd),
+                                      "Content-Type": "application/x-www-form-urlencoded",
+
+                                      # Some extra headers for fun
+                                      "Accept": "*/*", # curl does this
+                                      "User-Agent": "check_version/1", # otherwise it uses "Python-urllib/..."
+                                  })
+
+            e = (urllib2.urlopen(req)).read()
+            e_json = json.loads(e)
+            for record in e_json['op']['samples']:
+                name = ""
+                metric_type=""
+                stat = ""
                 _node = convert_ip_to_string(node)
-                for record in  b_json['op']['samples']:
-                    for idx, datapoint in enumerate(b_json['op']['samples'][record]):
-                        bucket_info['metrics'].append("{} {{bucket=\"{}\", node=\"{}\", type=\"bucket\"}} {} {}".format(record, bucket['name'], _node, datapoint, b_json['op']['samples']['timestamp'][idx]))
-    except Exception as e:
-        pass
-    return bucket_info
-
-
+                try:
+                    split_record = record.split("/")
+                    if len(split_record) == 3:
+                        name = (split_record[1]).replace("+", "_")
+                        metric_type = (split_record[2]).replace("+", "_")
+                        if type(e_json['op']['samples'][record]) == type([]):
+                            for idx, datapoint in enumerate(e_json['op']['samples'][record]):
+                                eventing_metrics['metrics'].append("{} {{node = \"{}\", function=\"{}\", type=\"eventing_stat\"}} {} {}".format(metric_type, _node, name, datapoint, e_json['op']['samples']['timestamp'][idx]))
+                        else:
+                            eventing_metrics['metrics'].append("{} {{node = \"{}\", function=\"{}\", type=\"eventing_stat\"}} {}".format(metric_type, _node, name, e_json['op']['samples'][record]))
+                    elif len(split_record) == 2:
+                        metric_type = (split_record[1]).replace("+", "_")
+                        if type(e_json['op']['samples'][record]) == type([]):
+                            for idx, datapoint in enumerate(e_json['op']['samples'][record]):
+                                eventing_metrics['metrics'].append("{} {{node = \"{}\", type=\"eventing_stat\"}} {} {}".format(metric_type, _node, datapoint, e_json['op']['samples']['timestamp'][idx]))
+                        else:
+                            eventing_metrics['metrics'].append("{} {{node = \"{}\", type=\"eventing_stat\"}} {}".format(metric_type, _node, datapoint))
+                    else:
+                        next
+                except Exception as e:
+                    print("eventing base: " + str(e))
+        except Exception as e:
+            print("eventing: " + str(e))
+    return eventing_metrics
 
 if __name__ == "__main__":
     url = "10.112.192.101"
@@ -216,14 +375,24 @@ if __name__ == "__main__":
     passwrd = "password"
 
     metrics = []
+
     clusterValues = _getCluster(url, user, passwrd)
     metrics = clusterValues['metrics']
+
     nodeMetrics = _getCluster(url, user, passwrd, clusterValues['nodeList'])
     metrics = metrics + nodeMetrics['metrics']
 
-    bucketMetrics = _get_bucket_metrics(url, user, passwrd, clusterValues['nodeList'])
-
+    bucketMetrics = _get_bucket_metrics(url, user, passwrd, clusterValues['serviceNodes']['kv'])
     metrics = metrics + bucketMetrics['metrics']
 
-    for metric in bucketMetrics['metrics']:
+    indexMetrics = _get_index_metrics(url, user, passwrd, clusterValues['serviceNodes']['index'], bucketMetrics['buckets'])
+    metrics = metrics + indexMetrics['metrics']
+
+    queryMetrics = _get_query_metrics(url, user, passwrd, clusterValues['serviceNodes']['n1ql'])
+    metrics = metrics + queryMetrics['metrics']
+
+    eventingMetrics = _get_eventing_metrics(url, user, passwrd, clusterValues['serviceNodes']['eventing'])
+    metrics = metrics + eventingMetrics['metrics']
+
+    for metric in eventingMetrics['metrics']:
         print(metric)
