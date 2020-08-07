2dfddc4b3fa3449baed70b5a6f935a5361f34e53
Tyler Denton <tyler.denton@couchbase.com>
2020-08-07 13:39:56 -0600

    Added remote controller for ssh

---
+++ src/application/modules/remote_control.py
@@ -0,0 +1,55 @@
+import sys
+import os
+import subprocess
+import json
+
+class SSH_controller():
+    def __init__(self, service, key, username, cb_username=None, cb_pw=None):
+        self.service = service
+        self.key = key
+        self.username = username
+        self.node = None
+        self.bucket = None
+        self.cb_username = cb_username
+        self.cb_pw = cb_pw
+        self.ssh = None
+        self.command = None
+
+    def get_connection(self, node = None, bucket = None, path=None):
+        self.node = node
+        self.bucket = bucket
+        self.path = path
+
+        if self.service == "cbstats":
+            self.command = " ".join([self.path,
+                        '{}:11210'.format(self.node),
+                        '-u', self.cb_username,
+                        '-p', self.cb_pw,
+                        '-b', bucket,
+                        '-j',
+                        'all'])
+            self.ssh = subprocess.Popen(["ssh", "-i", self.key, "-o", "StrictHostKeyChecking=no",
+                                            "-o", "PasswordAuthentication=no",
+                                            "{}@{}".format(self.username,
+                                                            self.node),
+                                            self.command],
+                                        shell=False,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+            self.result = self.ssh.stdout.readlines()
+            self.error = self.ssh.stderr.readlines()
+            if self.result == []:
+                print("{} had an error: {}".format(self.node, self.error))
+            else:
+                try:
+                    # python 3
+                    self.result = json.loads("".join([str(i, 'utf-8') for i in self.result]))
+                except:
+                    # python 2
+                    self.result = json.loads("".join([i.encode("utf-8") for i in self.result]))
+
+            return self.result
+
+if __name__ == '__main__':
+    jim = ssh_controller("cbstats", "~/.ssh/tdenton", "vagrant", "Administrator", "password")
+    print(jim.get_connection("10.112.194.101", "IdentityStore", "/opt/couchbase/bin/cbstats"))
